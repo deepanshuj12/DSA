085f8de1d0fa5dc780860024d8d5bb716e482deb
deepanshuj12 <ddeepanshuj29@gmail.com>
2026-06-06 17:36:57 +0530

    Time: 59 ms (35.22%) | Memory: 31.3 MB (90.95%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.py
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.py
@@ -1,20 +1,31 @@
 class Solution:
     def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
         arr.sort()
-        k=1
-        minn=float('inf')
-        lent=len(arr)
-        nums=[]
-        for i in range(lent):
-            if k>lent-1:
-                break
-            minn= min(minn,arr[k]-arr[i])
-            k=k+1
-        k=1
-        for i in range(lent):
-            if k>lent-1:
-                break
-            if arr[k]-arr[i]==minn:
-                nums.append([arr[i],arr[k]])
-            k=k+1
-        return nums
+
+        minn = min(arr[i + 1] - arr[i] for i in range(len(arr) - 1))
+
+        return [
+            [arr[i], arr[i + 1]]
+            for i in range(len(arr) - 1)
+            if arr[i + 1] - arr[i] == minn
+        ]
+# class Solution:
+#     def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+#         arr.sort()
+#         k=1
+#         minn=float('inf')
+#         lent=len(arr)
+#         nums=[]
+#         for i in range(lent):
+#             if k>lent-1:
+#                 break
+#             minn= min(minn,arr[k]-arr[i])
+#             k=k+1
+#         k=1
+#         for i in range(lent):
+#             if k>lent-1:
+#                 break
+#             if arr[k]-arr[i]==minn:
+#                 nums.append([arr[i],arr[k]])
+#             k=k+1
+#         return nums
