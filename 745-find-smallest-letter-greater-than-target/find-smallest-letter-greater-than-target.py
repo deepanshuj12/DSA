8623dfae460eead5aa9738a2a3f863443eacd0ec
deepanshuj12 <ddeepanshuj29@gmail.com>
2026-06-09 21:49:23 +0530

    Time: 2 ms (16.47%) | Memory: 20.6 MB (52.40%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.py
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.py
@@ -1,25 +1,19 @@
-# class Solution:
-#     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        
-#         term=''
-#         for i in range(len(letters)):
-#             if ord(letters[i])>ord(target):
-#                 term=letters[i]
-#                 break
-#         if term!='':
-#             return term
-#         else:
-#             return letters[0]
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        left = 0
-        right = len(letters)
-        while left < right:
-            mid = (left + right) // 2
-            if letters[mid] > target:
-                right = mid
-            else:
-                left = mid + 1
-        if left == len(letters):
-            return letters[0]
-        return letters[left]
+        for i in range(len(letters)):
+            if letters[i]>target:
+                return letters[i]
+        return letters[0]
+
+
+
+
+        # term=''
+        # for i in range(len(letters)):
+        #     if ord(letters[i])>ord(target):
+        #         term=letters[i]
+        #         break
+        # if term!='':
+        #     return term
+        # else:
+        #     return letters[0]
