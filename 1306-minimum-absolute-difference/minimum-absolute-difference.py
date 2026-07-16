26a3a972df11cb739e621e984409232ed444f635
deepanshuj12 <ddeepanshuj29@gmail.com>
2026-07-16 18:27:36 +0530

    Remove unused Solution class and method
    Removed commented-out class and method for minimum absolute difference.

--- 1306-minimum-absolute-difference/minimum-absolute-difference.py
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.py
@@ -9,23 +9,3 @@ class Solution:
             for i in range(len(arr) - 1)
             if arr[i + 1] - arr[i] == minnn
         ]
-# class Solution:
-#     def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
-#         arr.sort()
-#         k=1
-#         minn=float('inf')
-#         lent=len(arr)
-#         nums=[]
-#         for i in range(lent):
-#             if k>lent-1:
-#                 break
-#             minn= min(minn,arr[k]-arr[i])
-#             k=k+1
-#         k=1
-#         for i in range(lent):
-#             if k>lent-1:
-#                 break
-#             if arr[k]-arr[i]==minn:
-#                 nums.append([arr[i],arr[k]])
-#             k=k+1
-#         return nums
